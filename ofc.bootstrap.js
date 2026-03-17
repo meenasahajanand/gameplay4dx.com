32701ebaece5f26a33837544a62fb50d469fbbf9
ashrafSahajanand <ashraf.sahajanand@gmail.com>
2026-03-17 10:28:53 +0530

    chnge awlabel

--- ofc.bootstrap.js
+++ ofc.bootstrap.js
@@ -115,7 +115,7 @@ function gtag_report_conversion(url) {
     }
   };
   gtag("event", "conversion", {
-    send_to: "AW-17969480503/JsJMCKith_0bELeGwvh",
+    send_to: "AW-17830618655/49eTCIejhtcbEJ_MprZC",
     value: 0.1,
     currency: "AED",
     event_callback: callback,
@@ -373,7 +373,7 @@ if (typeof gtag === "undefined") {
     },
   });
 
-  gtag("config", "AW-17969480503");
+  gtag("config", "AW-17830618655");
 
   const gaScript = document.createElement("script");
   gaScript.async = true;
